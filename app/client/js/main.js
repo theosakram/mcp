e7864cfd3ffca9f64801143115d78aec8092b749
theosakram <wyrdhn@gmail.com>
2021-06-02 01:11:26 +0700

    init client

--- app/client/js/main.js
+++ app/client/js/main.js
@@ -1,9 +1,67 @@
 export class El extends Array {
-	ready() {}
+	ready(cb) {
+		const isReady = this.some((e) => {
+			return e.readyState !== null && e.readyState !== "loading";
+		});
 
-	listen() {}
+		if (isReady) {
+			cb();
+		} else {
+			this.listen("DOMContentLoaded", cb);
+		}
+
+		return this;
+	}
+
+	listen(event, cbOrSelector, cb) {
+		if (typeof cbOrSelector === "function") {
+			this.forEach((e) => e.addEventListener(event, cbOrSelector));
+		} else {
+			this.forEach((element) => {
+				element.addEventListener(event, (e) => {
+					if (e.target.match(cbOrSelector)) {
+						cb(e);
+					}
+				});
+			});
+		}
+
+		return this;
+	}
 }
 
-function d(params) {}
+function d(params) {
+	if (typeof params === "string" || params instanceof String) {
+		return new El(...document.querySelectorAll(params));
+	} else {
+		return new El(params);
+	}
+}
 
-d.get = async function () {};
+d.get = async function ({
+	url,
+	data = {},
+	success = () => {},
+	err = () => {},
+	done = () => {},
+}) {
+	const queryString = Object.entries(data)
+		.map(([key, value]) => {
+			return `${key}=${value}`;
+		})
+		.join("&");
+
+	try {
+		const res = await (
+			await fetch(`${url}?${queryString}`, {
+				method: "GET",
+			})
+		).json();
+
+		success(res);
+	} catch (error) {
+		err(error);
+	} finally {
+		done();
+	}
+};
