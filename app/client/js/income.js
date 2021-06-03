0463459f402394f16cf506ff3d5e8973e1ce32e0
theosakram <wyrdhn@gmail.com>
2021-06-03 18:45:12 +0700

    finishing css for tablet view

--- app/client/js/income.js
+++ app/client/js/income.js
@@ -1,47 +1,104 @@
-d.get({
-	url: baseUrl + "/income/8",
-	success: (data) => {
-		const tableBodyEl = d("#income-body")[0];
-
-		data.forEach((datum) => {
-			tableBodyEl.chainableAppendChild(
+const appendTable = (table, datum) => {
+	table.chainableAppendChild(
+		document
+			.createElement("tr")
+			.chainableAppendChild(
 				document
-					.createElement("tr")
+					.createElement("td")
+					.chainableAppendChild(document.createTextNode(datum.id))
+			)
+			.chainableAppendChild(
+				document
+					.createElement("td")
+					.chainableAppendChild(document.createTextNode(datum.source))
+			)
+			.chainableAppendChild(
+				document
+					.createElement("td")
 					.chainableAppendChild(
-						document
-							.createElement("td")
-							.chainableAppendChild(
-								document.createTextNode(datum.id)
-							)
+						document.createTextNode(
+							`Rp ${datum.amount.toLocaleString("id-ID")}`
+						)
 					)
+			)
+			.chainableAppendChild(
+				document
+					.createElement("td")
+					.chainableAppendChild(
+						document.createTextNode(
+							`${new Date(datum.createdAt).toLocaleDateString(
+								"id-ID"
+							)}`
+						)
+					)
+			)
+	);
+};
+
+const renderTablet = (div, datum, index) => {
+	div.chainableAppendChild(
+		document
+			.createElement("article")
+			.chainableAppendChild(
+				document
+					.createElement("p")
+					.chainableAppendChild(document.createTextNode(index + 1))
+			)
+			.chainableAppendChild(
+				document
+					.createElement("p")
+					.chainableAppendChild(document.createTextNode("Sumber: "))
 					.chainableAppendChild(
 						document
-							.createElement("td")
+							.createElement("span")
 							.chainableAppendChild(
 								document.createTextNode(datum.source)
 							)
 					)
+			)
+			.chainableAppendChild(
+				document
+					.createElement("p")
+					.chainableAppendChild(document.createTextNode("Jumlah: "))
 					.chainableAppendChild(
 						document
-							.createElement("td")
+							.createElement("span")
 							.chainableAppendChild(
-								document.createTextNode(
-									`Rp ${datum.amount.toLocaleString("id-ID")}`
-								)
+								document.createTextNode(datum.amount)
 							)
 					)
+			)
+			.chainableAppendChild(
+				document
+					.createElement("p")
+					.chainableAppendChild(document.createTextNode("Tanggal: "))
 					.chainableAppendChild(
 						document
-							.createElement("td")
+							.createElement("span")
 							.chainableAppendChild(
 								document.createTextNode(
-									`${new Date(
+									new Date(
 										datum.createdAt
-									).toLocaleDateString("id-ID")}`
+									).toLocaleDateString("id-ID")
 								)
 							)
 					)
-			);
+			)
+	);
+};
+
+d.get({
+	url: baseUrl + "/income/8",
+	success: (data) => {
+		const tableBodyEl = d("#income-body")[0];
+		const onTabletEl = d("#income-tablet")[0];
+
+		data.forEach((datum) => {
+			appendTable(tableBodyEl, datum);
+		});
+
+		data.forEach((datum, index) => {
+			renderTablet(onTabletEl, datum, index);
 		});
 	},
 	err: (error) => console.log(error),
